e7ffb3efe631079250b393bc470f021257e87fa5
mohamadasadizade <99049542+mohamadasadizade@users.noreply.github.com>
2022-04-05 19:13:40 +0430

    Add files via upload

---
+++ PDB-multichain-renumbering.py
@@ -0,0 +1,38 @@
+
+"""
+author: Mohammad Assadizadeh
+ 
+"""
+
+def readfile(filepath):
+    with open(filepath) as f:
+        return [ i.strip() for i in f.readlines()]
+    
+
+
+def rearranged_pdb_list(pdb_list , outputPath):
+    output = open(outputPath ,'w')
+    for i in range(len(pdb_list)):
+        if i >= 608 and i <= 4056 :
+            if pdb_list[i][21] == 'B':
+                new_line = pdb_list[i][:23] + str(int(pdb_list[i][23:26])+407) + pdb_list[i][26:]
+                output.write(new_line+'\n')
+            else:
+                output.write(pdb_list[i]+'\n')
+        else:
+            output.write(pdb_list[i]+'\n')
+    output.close()    
+    return output 
+
+
+rearranged_pdb_list(readfile('C:/Users/Mohammad/Desktop/7jtl (1).pdb'),'C:/Users/Mohammad/Desktop/7jtl.pdb')
+
+
+
+
+
+
+    
+
+
+
